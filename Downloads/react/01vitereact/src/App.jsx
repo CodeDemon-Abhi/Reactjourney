ea182a064e26706d746a72f4a69017f6e691b48e
Abhishek <www.abhi930@gmail.com>
2024-10-06 14:49:05 +0530

    movie project added

---
+++ Downloads/react/01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+  return (
+  <h1>Chai Aur Code with Vite |Abhishek | I just started learning Reactjs from one of the best teacher out there Hitesh Choudhary "Channel Name: Chai Aur Code"
+  </h1>
+  );
+}
+
+export default App;
